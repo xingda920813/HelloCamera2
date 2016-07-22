bb542fe1423ea1ebe1652f6f18e3c94db698f73e
Da Xing <d.x@fowtech.com>
2016-07-23 02:40:03 +0800

    压缩时寻找最佳分辨率的算法调整.

--- app/src/main/java/com/xdandroid/hellocamera2/util/BitmapUtils.java
+++ app/src/main/java/com/xdandroid/hellocamera2/util/BitmapUtils.java
@@ -18,6 +18,7 @@ public class BitmapUtils {
      * 将拍照得到的图片按照取景框（亮色区域）的范围进行裁剪.
      * 对于1280x720的屏幕，裁剪起始点为坐标(52, 80)，裁剪后，位图尺寸为896x588.（由layout xml定义的布局计算得到）
      * 以上参数将按照图片的实际大小，进行等比例换算。
+     *
      * @param originalBitmap 拍照得到的Bitmap
      * @return 裁剪之后的Bitmap
      */
@@ -39,6 +40,7 @@ public class BitmapUtils {
     /**
      * 若图片宽小于高，则逆时针旋转90° ; 否则，返回原图片.
      * 适用于调用系统相机进行拍照，且希望图片总是横向的场景。
+     *
      * @param sourceBitmap 拍照得到的Bitmap
      * @return 若图片宽小于高，返回逆时针旋转90°后的Bitmap ; 否则，返回原Bitmap.
      */
@@ -59,8 +61,10 @@ public class BitmapUtils {
     /**
      * 将图片文件压缩到所需的大小，返回位图对象.
      * 若原图尺寸小于需要压缩到的尺寸，则返回原图.
-     * @param filePath 图片File
-     * @param reqWidth 压缩后的宽度
+     * 该方法不处理图片方向问题，需要确保File的长宽比与req中的比例相近.
+     *
+     * @param filePath  图片File
+     * @param reqWidth  压缩后的宽度
      * @param reqHeight 压缩后的高度
      * @return 压缩后的Bitmap. 若原图尺寸小于需要压缩到的尺寸，则返回File解码得到的Bitmap.
      */
@@ -80,9 +84,10 @@ public class BitmapUtils {
     /**
      * 将图片文件压缩到所需的大小，返回位图对象.
      * 若原图尺寸小于需要压缩到的尺寸，则返回原图.
-     * 该方法考虑了可能需要在压缩后进行旋转的情况，因此可放心传入reqWidth和reqHeight，而无需考虑图片方向.
-     * @param filePath 图片File
-     * @param reqWidth 压缩后的宽度
+     * 该方法考虑了图片方向问题，因此可始终传入reqWidth > reqHeight.
+     *
+     * @param filePath  图片File
+     * @param reqWidth  压缩后的宽度
      * @param reqHeight 压缩后的高度
      * @return 压缩后的Bitmap. 若原图尺寸小于需要压缩到的尺寸，则返回File解码得到的Bitmap.
      */
@@ -106,27 +111,77 @@ public class BitmapUtils {
     }
 
     /**
-     * 计算压缩参数BitmapFactory.Options.inSampleSize
-     * @param options BitmapFactory.Options
-     * @param reqWidth 压缩后的宽度
+     * 将图片文件压缩到所需的大小，返回位图对象.
+     * 若原图尺寸小于需要压缩到的尺寸，则返回原图.
+     * 该方法通过分辨率面积得到inSampleSize，因此不存在图片方向问题.
+     *
+     * @param filePath
+     * @param reqSquarePixels
+     * @return
+     */
+    public static Bitmap compressToResolution(File filePath, long reqSquarePixels) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(filePath.toString(), options);
+        options.inSampleSize = calculateInSampleSizeBySquare(options, reqSquarePixels);
+        options.inJustDecodeBounds = false;
+        //inSampleSize通过SubSampling实现，只是节省了读入Bitmap后占用的内存，Bitmap本身的像素还是那么多，文件体积不会减小。
+        Bitmap inSampleSizedBitmap = BitmapFactory.decodeFile(filePath.toString(), options);
+        Bitmap scaledBitmap = Bitmap.createScaledBitmap(inSampleSizedBitmap, options.outWidth / options.inSampleSize, options.outHeight / options.inSampleSize, false);
+        inSampleSizedBitmap.recycle();
+        return scaledBitmap;
+    }
+
+    /**
+     * 计算压缩参数BitmapFactory.Options.inSampleSize.
+     * 考虑了options的长宽比可能与req中的比例不同的情况.
+     * 该方法通过分辨率面积得到inSampleSize，因此不存在图片方向问题.
+     *
+     * @param options   BitmapFactory.Options
+     * @param reqSquarePixels  压缩后的分辨率面积
+     * @return 计算得到的BitmapFactory.Options.inSampleSize
+     */
+    private static int calculateInSampleSizeBySquare(BitmapFactory.Options options, long reqSquarePixels) {
+        long squarePixels = options.outWidth * options.outHeight;
+        if (squarePixels <= reqSquarePixels) return 1;
+        double powwedScale = ((double) squarePixels) / ((double) reqSquarePixels);
+        double scale = Math.sqrt(powwedScale);
+        double log = Math.log(scale) / Math.log(2);
+        double logCeil = Math.ceil(log);
+        return (int) Math.pow(2, logCeil);
+    }
+
+    /**
+     * 计算压缩参数BitmapFactory.Options.inSampleSize.
+     * 考虑了options中的长宽比可能与req的长宽比不同的情况.
+     *
+     * @param options   BitmapFactory.Options
+     * @param reqWidth  压缩后的宽度
      * @param reqHeight 压缩后的高度
      * @return 计算得到的BitmapFactory.Options.inSampleSize
      */
     private static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
         int width = options.outWidth;
         int height = options.outHeight;
-        if (width <= reqWidth && height <= reqHeight) {
-            return 1;
-        } else {
-            double scale = width >= height ? width / reqWidth : height / reqHeight;
-            double log = Math.log(scale) / Math.log(2);
-            double logCeil = Math.ceil(log);
-            return (int) Math.pow(2, logCeil);
-        }
+        double ratio = ((double) width) / ((double) height);
+        double reqRatio = ((double) reqWidth) / ((double) reqHeight);
+        /**
+         * 16:9 -> 4:3 : 3840x2160 -> 1600x1200 : 取height之比作为scale
+         * 3:4 -> 9:16 : 2400x3200 -> 1080x1920 : 取height之比作为scale
+         * 9:16 -> 3:4 : 2160x3840 -> 1200x1600 : 取width之比作为scale
+         * 4:3 -> 16:9 : 3200x2400 -> 1920x1080 : 取width之比作为scale
+         */
+        boolean shouldUseWidthScale = ratio <= reqRatio;
+        if (width <= reqWidth && height <= reqHeight) return 1;
+        double scale = shouldUseWidthScale ? ((double) width) / ((double) reqWidth) : ((double) height) / ((double) reqHeight);
+        double log = Math.log(scale) / Math.log(2);
+        double logCeil = Math.ceil(log);
+        return (int) Math.pow(2, logCeil);
     }
 
     /**
      * BitmapFactory.decodeFile(String pathName, Options opts)的快捷方法.
+     *
      * @param filePath 图片File
      * @return 解码文件得到的Bitmap.
      */
@@ -136,7 +191,8 @@ public class BitmapUtils {
 
     /**
      * 将Bitmap写入文件，文件位于外置存储上该应用包名目录的cache子目录中.
-     * @param bitmap 要写入的Bitmap
+     *
+     * @param bitmap   要写入的Bitmap
      * @param fileName 文件名
      * @return 文件对应的File.
      */
